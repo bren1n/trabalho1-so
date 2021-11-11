3acd2cc27cad44c5c93d2f2607733061e3667d0c
Breno Ferreira <brenoleite1234@gmail.com>
2021-11-10 21:49:57 -0300

    Cria programa auxiliar e implementaçao sequencial para multiplicaçao de matrizes

---
+++ src/auxiliar.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+
+int main(int argc, char *argv[]) {
+  //Armazena dimensões
+  int n1 = atoi(argv[1]);
+  int m1 = atoi(argv[2]);
+  int n2 = atoi(argv[3]);
+  int m2 = atoi(argv[4]);
+
+  // Gera elementos aleatórios
+  vector<vector<int>> matrix1;
+
+  srand((int) time(0));
+  for (int i = 0; i < n1; i++) {
+    matrix1.push_back(vector<int>());
+    for (int j = 0; j < m1; j++) {
+      matrix1[i].push_back(rand() % 10 + 1);
+    }
+  }
+
+  vector<vector<int>> matrix2;
+
+  for (int i = 0; i < n2; i++) {
+    matrix2.push_back(vector<int>());
+    for (int j = 0; j < m2; j++) {
+      matrix2[i].push_back(rand() % 10 + 1);
+    }
+  }
+  
+  //Armazena matrizes nos arquivos
+  fstream file;
+  file.open("data/m1.txt", fstream::out);
+  file << n1 << ' ' << m1 << endl;
+
+  for (int i = 0; i < n1; i++) {
+    for (int j = 0; j < m1; j++) {
+      file << matrix1[i][j] << (j < m1 - 1 ? ' ' : endl);
+    }
+  }
+
+  file.close();
+
+  file.open("data/m2.txt", fstream::out);
+  file << n2 << ' ' << m2 << endl;
+
+  for (int i = 0; i < n2; i++) {
+    for (int j = 0; j < m2; j++) {
+      file << matrix2[i][j] << (j < m2 - 1 ? ' ' : endl);
+    }
+  }
+
+  file.close();
+
+  return 0;
+}
---
+++ src/sequencial.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+
+int main(int argc, char *argv[]) {
+  int n1, m1, n2, m2, aux;
+  vector<vector<int>> matrix1, matrix2, result;
+  
+  
+  // Preenche matrizes antes de multiplicar
+  fstream file;
+  file.open(argv[1]);  
+
+  file >> n1;
+  file >> m1;
+
+  for (int i = 0; i < n1; i++) {
+    matrix1.push_back(vector<int>());
+    for (int j = 0; j < m1; j++) {
+      file >> aux;
+      matrix1[i].push_back(aux);
+    }
+  }
+
+  file.close();
+
+  file.open(argv[2]);  
+
+  file >> n2;
+  file >> m2;
+
+  for (int i = 0; i < n2; i++) {
+    matrix2.push_back(vector<int>());
+    for (int j = 0; j < m2; j++) {
+      file >> aux;
+      matrix2[i].push_back(aux);
+    }
+  }
+
+  file.close();
+
+  //Multiplica matrizes
+  for (int i = 0; i < n1; i++) {
+    result.push_back(vector<int>());
+    for (int j = 0; j < m2; j++) {
+      result[i].push_back(0);
+      for (int k = 0; k < m1; k++) {
+        result[i][j] += matrix1[i][k] * matrix2[k][j];
+      }
+    }
+  }
+
+  //Salva resultado em arquivo
+  file.open("data/result_sequencial.txt", fstream::out);
+  file << n1 << ' ' << m2 << endl;
+  
+  for (int i = 0; i < n1; i++) {
+    for (int j = 0; j < m2; j++) {
+      file << 'c' << i+1 << j+1 << ' ' << result[i][j] << endl;
+    }
+  }
+
+  file.close();
+
+  return 0;
+}
